7ce378958facf9d9c0ba3391b1c4b678e3bece18
Siddhesh <siddheshzeus@gmail.com>
2023-10-10 16:48:37 +0530

    first commit

---
+++ Async&Await/test.js
@@ -0,0 +1,56 @@
+const orderId = 7000;
+
+const Promisez = ValueChecker(orderId);
+console.log(Promisez);
+Promisez.then((result) => {
+    console.log(result);
+    const gstval=gstCalculate(result);
+    return gstval
+}).then(function(x){
+    console.log(x);
+}).catch(function(err2){
+    console.log(err2.message);
+})
+.catch((err) => {
+    console.log(err.message);
+});
+
+function gstCalculate(val){
+    console.log("Value inside Gst function is:",val);
+    const pypro = new Promise((resolve,reject)=>{
+        if(val>6000){
+            let bk=val+(val*0.18);
+            console.log("GST Added Succesfully.");
+            resolve(bk);
+        }
+        else{
+            let err2 = new Error("Gst cannot be applied Minimum amount needed is 6000")
+            reject(err2);
+        }
+        console.log(pypro);
+    })
+    return pypro;
+}
+
+function ValueChecker(oid){
+    const rez = new Promise(function(resolve,reject ){
+        if(oid<4000){
+            const er = new Error("AI Error");
+            reject(er);
+        }
+        
+        if(oid>4500){
+            setTimeout(()=>{
+                resolve(oid);
+            },4000)
+        }
+    })
+    return rez;
+}
+
+
+
+
+setTimeout(()=>{console.log(Promisez);},6000);
+
+
---
+++ Tic_tac_toe/script.js
@@ -0,0 +1,9 @@
+let comp_ch = document.getElementById('c_choice');
+let user_ch = document.getElementById('y_choice');
+let fn_result= document.getElementById('f_result');
+
+let pos_choices = document.querySelectorAll('button');
+
+
+
+ 
